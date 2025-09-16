e7025495dff7c0783da3c5bd7e1d35337aad5df1
Cupra85 <daniel.curten@gmail.com>
2025-09-16 21:24:35 +0200

    Update __init__.py

--- components/vitoconnect/sensor/__init__.py
+++ components/vitoconnect/sensor/__init__.py
@@ -1,11 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_NAME, CONF_ADDRESS, CONF_LENGTH, CONF_UPDATE_INTERVAL
+from esphome.const import CONF_ID, CONF_NAME, CONF_ADDRESS, CONF_LENGTH #, CONF_TYPE 
 from .. import vitoconnect_ns, VitoConnect, CONF_VITOCONNECT_ID
 
 DEPENDENCIES = ["vitoconnect"]
-
 OPTOLINKSensor = vitoconnect_ns.class_("OPTOLINKSensor", sensor.Sensor)
 
 CONFIG_SCHEMA = sensor.sensor_schema(OPTOLINKSensor).extend({
@@ -13,7 +12,6 @@ CONFIG_SCHEMA = sensor.sensor_schema(OPTOLINKSensor).extend({
     cv.GenerateID(CONF_VITOCONNECT_ID): cv.use_id(VitoConnect),
     cv.Required(CONF_ADDRESS): cv.uint16_t,
     cv.Required(CONF_LENGTH): cv.uint8_t,
-    cv.Optional(CONF_UPDATE_INTERVAL, default="60s"): cv.positive_time_period_milliseconds,
 })
 
 async def to_code(config):
@@ -22,7 +20,6 @@ async def to_code(config):
     # Add configuration to datapoint
     cg.add(var.setAddress(config[CONF_ADDRESS]))
     cg.add(var.setLength(config[CONF_LENGTH]))
-    cg.add(var.setPollInterval(config[CONF_UPDATE_INTERVAL]))
 
     # Add sensor to component hub (VitoConnect)
     hub = await cg.get_variable(config[CONF_VITOCONNECT_ID])
