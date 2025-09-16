b65f4537f2bde809e36a685c684c3c8b8f427beb
Cupra85 <daniel.curten@gmail.com>
2025-09-16 21:14:41 +0200

    Update vitoconnect_datapoint.h

--- components/vitoconnect/vitoconnect_datapoint.h
+++ components/vitoconnect/vitoconnect_datapoint.h
@@ -2,30 +2,27 @@
 
 #include <cstdint>
 #include <functional>
-#include <cstring>   // memcpy, memset
+#include <cstring>
 
 namespace esphome {
 namespace vitoconnect {
 
 class Datapoint {
  public:
-  Datapoint();
-  virtual ~Datapoint();
+  Datapoint() = default;
+  virtual ~Datapoint() = default;
 
-  void setAddress(uint16_t address) { this->_address = address; }
-  uint16_t getAddress() const { return this->_address; }
+  void setAddress(uint16_t address) { _address = address; }
+  uint16_t getAddress() const { return _address; }
 
-  void setLength(uint8_t length) { this->_length = length; }
-  uint8_t getLength() const { return this->_length; }
+  void setLength(uint8_t length) { _length = length; }
+  uint8_t getLength() const { return _length; }
 
-  void setPollInterval(uint32_t interval) { this->_poll_interval = interval; }
-  uint32_t getPollInterval() const { return this->_poll_interval; }
+  void setPollInterval(uint32_t interval) { _poll_interval = interval; }
+  uint32_t getPollInterval() const { return _poll_interval; }
 
-  void setLastUpdate(uint32_t t) { this->_last_update = t; }
-  uint32_t getLastUpdate() const { return this->_last_update; }
-
-  static void onData(std::function<void(uint8_t[], uint8_t, Datapoint*)> callback);
-  void onError(uint8_t error, Datapoint* dp);
+  void setLastUpdate(uint32_t t) { _last_update = t; }
+  uint32_t getLastUpdate() const { return _last_update; }
 
   virtual void encode(uint8_t* raw, uint8_t length, void* data);
   virtual void decode(uint8_t* data, uint8_t length, Datapoint* dp = nullptr);
@@ -33,7 +30,7 @@ class Datapoint {
  protected:
   uint16_t _address{0};
   uint8_t _length{0};
-  uint32_t _poll_interval{60000};  // Standard: 60s
+  uint32_t _poll_interval{60000};
   uint32_t _last_update{0};
 
   static std::function<void(uint8_t[], uint8_t, Datapoint*)> _stdOnData;
