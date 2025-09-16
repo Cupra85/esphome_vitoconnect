0f8cb2510ac84ffc5f60ea9470598701378c3678
Cupra85 <daniel.curten@gmail.com>
2025-09-16 17:41:46 +0200

    Update __init__.py

--- components/vitoconnect/binary_sensor/__init__.py
+++ components/vitoconnect/binary_sensor/__init__.py
@@ -1,20 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_ADDRESS
+from esphome.const import CONF_ID, CONF_NAME, CONF_ADDRESS, CONF_LENGTH #, CONF_TYPE 
 from .. import vitoconnect_ns, VitoConnect, CONF_VITOCONNECT_ID
 
 DEPENDENCIES = ["vitoconnect"]
-
 OPTOLINKBinarySensor = vitoconnect_ns.class_("OPTOLINKBinarySensor", binary_sensor.BinarySensor)
 
-CONF_POLL_INTERVAL = "poll_interval"
-
-CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
+CONFIG_SCHEMA =  binary_sensor.BINARY_SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(OPTOLINKBinarySensor),
     cv.GenerateID(CONF_VITOCONNECT_ID): cv.use_id(VitoConnect),
-    cv.Required(CONF_ADDRESS): cv.uint16_t,
-    cv.Optional(CONF_POLL_INTERVAL, default="60s"): cv.positive_time_period_milliseconds,
+    cv.Required(CONF_ADDRESS): cv.uint16_t
 })
 
 async def to_code(config):
@@ -23,9 +19,7 @@ async def to_code(config):
     # Add configuration to datapoint
     cg.add(var.setAddress(config[CONF_ADDRESS]))
     cg.add(var.setLength(1))
-    cg.add(var.setPollInterval(config[CONF_POLL_INTERVAL]))
 
     # Add sensor to component hub (VitoConnect)
     hub = await cg.get_variable(config[CONF_VITOCONNECT_ID])
     cg.add(hub.register_datapoint(var))
-
