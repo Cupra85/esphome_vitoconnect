04ed93ead8746f12430412abdc00acd2f136f6f4
Cupra85 <daniel.curten@gmail.com>
2025-09-16 21:12:23 +0200

    Update vitoconnect.h

--- components/vitoconnect/vitoconnect.h
+++ components/vitoconnect/vitoconnect.h
@@ -1,40 +1,28 @@
 #pragma once
 
-#include "vitoconnect_datapoint.h"
 #include "esphome/core/component.h"
+#include "vitoconnect_datapoint.h"
 #include <vector>
 
 namespace esphome {
 namespace vitoconnect {
 
-class OptolinkBase;
-
-enum ProtocolType { KW, P300 };
-
 class VitoConnect : public PollingComponent {
  public:
-  VitoConnect() : PollingComponent(0), _optolink(nullptr), protocol(KW) {}
+  VitoConnect() : PollingComponent(1000) {}  // Default Polling 1s
 
   void setup() override;
   void loop() override;
   void update() override;
 
-  void register_datapoint(Datapoint *datapoint);
+  void register_datapoint(Datapoint* dp);
 
-  ProtocolType protocol;
-
- protected:
-  OptolinkBase* _optolink;
-  std::vector<Datapoint*> _datapoints;
-
-  struct CbArg {
-    VitoConnect* v;
-    Datapoint* dp;
-    CbArg(VitoConnect* vv, Datapoint* dd) : v(vv), dp(dd) {}
-  };
-
+  // Callbacks
   static void _onData(uint8_t* data, uint8_t len, void* arg);
   static void _onError(uint8_t error, void* arg);
+
+ protected:
+  std::vector<Datapoint*> _datapoints;
 };
 
 }  // namespace vitoconnect
