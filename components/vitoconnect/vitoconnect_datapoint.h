eba2d9b3163ece8c008a4f078d791853259629f9
Cupra85 <daniel.curten@gmail.com>
2025-09-16 21:00:05 +0200

    Update vitoconnect_datapoint.h

--- components/vitoconnect/vitoconnect_datapoint.h
+++ components/vitoconnect/vitoconnect_datapoint.h
@@ -1,45 +1,32 @@
-#pragma once
-
-#include <stdint.h>
-#include <functional>
-#include <string.h>
-#include "esphome/core/helpers.h"
+#include "vitoconnect_datapoint.h"
 
 namespace esphome {
 namespace vitoconnect {
 
-class Datapoint {
- public:
-  Datapoint() = default;
-  virtual ~Datapoint() = default;
+std::function<void(uint8_t[], uint8_t, Datapoint* dp)> Datapoint::_stdOnData = nullptr;
 
-  void setAddress(uint16_t address) { this->_address = address; }
-  uint16_t getAddress() const { return this->_address; }
+void Datapoint::onData(std::function<void(uint8_t[], uint8_t, Datapoint* dp)> callback) {
+  _stdOnData = callback;
+}
 
-  void setLength(uint8_t length) { this->_length = length; }
-  uint8_t getLength() const { return this->_length; }
+void Datapoint::encode(uint8_t* raw, uint8_t length, void* data) {
+  if (length != _length) {
+    memset(raw, 0, _length);
+  } else {
+    memcpy(raw, data, length);
+  }
+}
 
-  void setPollInterval(uint32_t interval) { this->_poll_interval = interval; }
-  uint32_t getPollInterval() const { return this->_poll_interval; }
-
-  void setLastUpdate(uint32_t t) { this->_last_update = t; }
-  uint32_t getLastUpdate() const { return this->_last_update; }
-
-  static void onData(std::function<void(uint8_t[], uint8_t, Datapoint* dp)> callback);
-  void onError(uint8_t, Datapoint* dp);
-
-  virtual void encode(uint8_t* raw, uint8_t length, void* data);
-  virtual void decode(uint8_t* data, uint8_t length, Datapoint* dp = nullptr);
-
- protected:
-  uint16_t _address{0};
-  uint8_t _length{0};
-
-  uint32_t _poll_interval{60000};
-  uint32_t _last_update{0};
-
-  static std::function<void(uint8_t[], uint8_t, Datapoint* dp)> _stdOnData;
-};
+void Datapoint::decode(uint8_t* data, uint8_t length, Datapoint* dp) {
+  uint8_t* output = new uint8_t[_length];
+  memset(output, 0, _length);
+  if (length == _length) {
+    memcpy(output, data, length);
+    if (_stdOnData) _stdOnData(output, _length, dp);
+    _last_update = esphome::millis();  // korrekt
+  }
+  delete[] output;
+}
 
 }  // namespace vitoconnect
 }  // namespace esphome
